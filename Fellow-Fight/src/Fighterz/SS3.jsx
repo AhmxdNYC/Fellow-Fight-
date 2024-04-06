f3295d2cc60440da92d555095506cf934cd7a4ad
ahmad <ahmadhamza052005@gmail.com>
2024-04-06 06:59:40 -0400

    testing cmd space x2

--- Fellow-Fight/src/Fighterz/SS3.jsx
+++ Fellow-Fight/src/Fighterz/SS3.jsx
@@ -20,7 +20,7 @@ class SS3 extends Fighter {
     this.animationSpeed = 18
     // 18 normal
   }
-//testing ing again nnklnmlksfsfsfsf test ishshhsfsfs big test 4 676899
+//testing ing again nnklnmlksfsfsfsf test ishshhsfsfs big test 4 676899 final test
   cycleFrames() {
     this.animationCounter++
     if (this.animationCounter >= this.animationSpeed) {
