5ca6b4beb34b746e74c6727e9f3d6ee874fcdf99
ahmad <ahmadhamza052005@gmail.com>
2024-04-06 06:35:37 -0400

    sfs print extra s

--- Fellow-Fight/src/Fighterz/SS3.jsx
+++ Fellow-Fight/src/Fighterz/SS3.jsx
@@ -20,7 +20,7 @@ class SS3 extends Fighter {
     this.animationSpeed = 18
     // 18 normal
   }
-//testing ing again nnklnmlksfsfsfsf test ishshhsfsf
+//testing ing again nnklnmlksfsfsfsf test ishshhsfsfs
   cycleFrames() {
     this.animationCounter++
     if (this.animationCounter >= this.animationSpeed) {
